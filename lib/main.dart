f5ed256df13028c8f8897127652ecc688391404b
Amit Kumar Ray <amitkumar8340441436@gmail.com>
2024-08-14 10:36:14 +0530

    day 1 make navigation button

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mausam_app/Activity/home.dart';
+import 'package:mausam_app/Activity/loading.dart';
+import 'package:mausam_app/Activity/location.dart';
+
+void main() {
+  runApp(MaterialApp(
+    // home: Home(),
+    routes: {
+      "/" : (context) => Loading(),
+      "/home" : (context) => Home(),
+    },
+  ));
+}
